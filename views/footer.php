57f911da07b97c6e371aa7ed5adeaf0616d3c585
Zeshan Segal <zeshan.segal@gmail.com>
2015-12-14 15:47:22 -0600

    added new login/logout pages css for footer, and footer pages

--- views/footer.php
+++ views/footer.php
@@ -6,7 +6,7 @@
 		<p class="text-muted">All rights reserved ZTR Industires</p>
 		<p>Email: <a href="mailto:someone@ztr.com">someone@ztr.com</a>.</p>
 		<p>Help: <a href="help@ztr.com">help@ztr.com</a>.</p>
-		<p>About: <a href="adlister.dev/about.php">About ZTR</a>ABOUT ZTR INDUSTRIES</p>
+		<p>About: <a href="adlister.dev/about.php">About ZTR</a></p>
 
 	</div>
 </footer>
