6b91c9b1fecb6798f4d8445d945c9328a81ac891
Anthony Burns <aburns42@gmail.com>
2015-12-14 14:18:40 -0600

    Updated README.md with a description of the exercise and added PHP require_once to index.php for the navbar and footer.

--- public/index.php
+++ public/index.php
@@ -17,9 +17,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>ZTR Industries Ad Lister 3000</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/footer.css">
         <link rel="stylesheet" href="../css/main.css">
     </head>
     <body>
+        <?php require_once 'navbar.php'; ?>
+
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
@@ -28,7 +31,19 @@
             </div> <!-- End row. --> 
 
             <div class="row">
-                <div class="col-md-10 col-md-offset-1">
+                <div class="col-md-2">
+                    <h4>Other Stuff of Some Import</h4>
+                    <form method="POST" action="index.php" class="form-horizontal">
+                        <div class="form-group">
+                            <label class="col-sm-5 control-label">Search</label>
+                            <div class="col-sm-6">
+                                <input type="text" name="description" value="..." class="form-control">
+                            </div>
+                        </div>
+                        <button class="btn btn-default" type="submit">Submit</button>
+                    </form>
+                </div> <!-- End col-md-12 -->
+                <div class="col-md-8">
                     <h3>Newest Ads</h3>
                     <ul class="list-group">
                         <li class="list-group-item"><img src="..." class="img-responsive" alt="Responsive image"><p>stuff and things.</p></li>
@@ -45,5 +60,7 @@
                 </div> <!-- End col-md-12 -->
             </div> <!-- End row. --> 
         </div> <!-- End container. -->
+
+        <?php require_once 'footer.php'; ?>
     </body>
 </html>
