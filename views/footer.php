6fac0568b23421c461143342070853728b2cf7e1
Zeshan Segal <zeshan.segal@gmail.com>
2015-12-14 14:38:57 -0600

    "Initial Commit"

---
+++ views/footer.php
@@ -0,0 +1,12 @@
+
+
+<footer class="footer">
+	<div class="container">
+
+		<p class="text-muted">All rights reserved ZTR Industires</p>
+		<p>Email: <a href="mailto:someone@ztr.com">someone@ztr.com</a>.</p>
+		<p>Help: <a href="help@ztr.com">help@ztr.com</a>.</p>
+		<p>About: <a href="adlister.dev/about.php">About ZTR</a>ABOUT ZTR INDUSTRIES</p>
+
+	</div>
+</footer>
