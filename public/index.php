8c57b878c8c66039422954a37368f3b4c09f551a
Anthony Burns <aburns42@gmail.com>
2015-12-14 14:03:23 -0600

    Updated files, added basic HTML content to index.php and a bootstrap file to the CSS folder.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php
 
-    function pageController()
-    {
+    // function pageController()
+    // {
         // PHP code goes here.    
-    }
+    // }
 
-    extract(pageController());
+    // extract(pageController());
 
 ?>
 
@@ -15,14 +15,35 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PHP HTML Template</title>
-        <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
+        <title>ZTR Industries Ad Lister 3000</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/x.css">
+        <link rel="stylesheet" href="../css/main.css">
     </head>
     <body>
-        <div class="main">
-            <!-- HTML goes here. -->
-        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>The ZTR Industries Ad Lister 3000</h2>
+                </div> <!-- End col-md-12 -->
+            </div> <!-- End row. --> 
+
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <h3>Newest Ads</h3>
+                    <ul class="list-group">
+                        <li class="list-group-item"><img src="..." class="img-responsive" alt="Responsive image"><p>stuff and things.</p></li>
+                        <li class="list-group-item"><img src="..." class="img-responsive" alt="Responsive image"><p>stuff and things.</p></li>
+                        <li class="list-group-item"><img src="..." class="img-responsive" alt="Responsive image"><p>stuff and things.</p></li>
+                    </ul>    
+
+                </div> <!-- End col-md-12 -->
+            </div> <!-- End row. --> 
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h4>Other Stuff of Some Import</h4>
+                </div> <!-- End col-md-12 -->
+            </div> <!-- End row. --> 
+        </div> <!-- End container. -->
     </body>
 </html>
