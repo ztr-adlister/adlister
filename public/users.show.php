e5626fda98d3b2177f1ae04ae28191730b36c309
Reagan Wilkins <reagan.wilkins@gmail.com>
2015-12-15 15:08:55 -0600

    Created Migration file Filled Profile page with mock info

--- public/users.show.php
+++ public/users.show.php
@@ -4,6 +4,56 @@
 2) Feed in user info from database.
 3) Make sure that the session that started on the login page is still running
 */
+function pageController()
+{
+    $user = [
+        [
+            'id' => 1,
+            'boxcolor' => 'blue',
+            'username' => 'R-fresh',
+            'email' => 'R-fresh@rfresh.com',
+            'password' => 'hello'
+        ]
+    ];
+    $userads = [
+        [
+            'id' => 1,
+            'name' => 'Ray Ban Sunglasses',
+            'description' => 'For a limited time, get these brand new Ray Bans!',
+            'price' => 200.00
+        ],
+        [
+            'id' => 2,
+            'name' => 'Used Chewing Gum',
+            'description' => 'I was chewing this gum and it lost its flavor...come on, man, I gotta pay rent!',
+            'price' => 1.00
+        ]
+    ];
+    $chosenads = [
+        [ 
+            'id' => 1,
+            'username' => 'XxcryingprincessrainxX',
+            'name' => 'Buncha Bananas',
+            'description' => 'Its a bunch of bananas! So many bananas!',
+            'price' => 5000.50
+        ],
+        [
+            'id' => 2,
+            'username' => 'theladieslovemeimnotalone9215098',
+            'name' => 'Best of the Dire Straights',
+            'description' => 'Best of the Dire Straights, now on a two disc set',
+            'price' => 100.30
+        ]
+    ];
+
+    return array (
+        'user' => $user,
+        'userads' => $userads,
+        'chosenads' => $chosenads
+    );
+}
+
+extract(pageController());
 ?>
 <!DOCTYPE html>
 <!-- Carried over from the index -->
@@ -20,27 +70,32 @@
     </head>
     <?php require_once '../views/navbar.php'; ?>
     <body>
-    	<h2 class = "show">Hello, {User}!</h2>
+        <?php foreach($user as $userdata) {?>
+    	<h2 class = "show">Hello, <?=$userdata['username']?>!</h2>
+        <?php } ?>
         <div id = "box"></div>
 
 <!-- Ads the user has posted -->
     	<h3 class = "show">Your Ads:</h3>
     	<ul class = "show">
-    		<li>Ad1</li>
-    		<li>Ad2</li>
-    		<li>Ad3</li>
-    		<li>Ad4</li>
-    		<li>Ad5</li>
+    		<?php foreach($userads as $advalue) {?>
+            <li><strong>Name:</strong> <?=$advalue['name']?></li>
+            <li><strong>Description:</strong> <?=$advalue['description']?></li>
+            <li><strong>Price:</strong> $<?=$advalue['price']?></li>
+            <br>
+            <?php } ?>
     	</ul>
 
 <!-- Ads the user has responded to -->
     	<h3 class = "show">Ads you have responded to:</h3>
     	<ul class = "show">
-    		<li>Ad1</li>
-    		<li>Ad2</li>
-    		<li>Ad3</li>
-    		<li>Ad4</li>
-    		<li>Ad5</li>
+    		<?php foreach($chosenads as $chosenthings) {?>
+            <li><strong>Name:</strong> <?=$chosenthings['name']?></li>
+            <li><strong>Description:</strong> <?=$chosenthings['description']?></li>
+            <li><strong>Price:</strong> $<?=$chosenthings['price']?></li>
+            <li><strong>Offered by:</strong> <?=$chosenthings['username']?></li>
+            <br>
+            <?php } ?>
     	</ul>
     	<br>
 <!-- Takes you to the edit profile page -->
