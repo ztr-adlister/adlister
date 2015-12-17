f2dcf3c3e216d096d4d14e2eeced5030d945aa4f
Anthony Burns <aburns42@gmail.com>
2015-12-17 15:04:50 -0600

    Updated files, got the ad update page working.

--- models/Ad.php
+++ models/Ad.php
@@ -5,6 +5,16 @@ require_once 'Basemodel.php';
 class Ad extends Model
 {
 	protected static $table= 'ads';
+
+	public static function findAllAdsByUserId($user_id)
+	{
+		self::dbConnect();
+		$stmt = self::$dbc->prepare('SELECT * FROM ' . static::$table . ' WHERE user_id = :user_id');
+		$stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+		$stmt->execute();
+		$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $results;
+	}
 }
 
 
