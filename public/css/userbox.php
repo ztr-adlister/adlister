97e7b0dab8d8dbadb12fac16a5032ffa183e0988
Reagan Wilkins <reagan.wilkins@gmail.com>
2015-12-16 18:32:06 -0600

    The box on your profile is now whatever color you had it set as! Try it out!

---
+++ public/css/userbox.php
@@ -0,0 +1,15 @@
+<?php
+$boxdata = $dbc->prepare('SELECT boxcolor FROM users WHERE username = :username');
+$boxdata->bindValue(':username', $_SESSION['Loggedinuser'], PDO::PARAM_STR);
+$boxdata->execute();
+$boxcolor = $boxdata->fetch(PDO::FETCH_ASSOC);
+?>
+<style>
+#box {
+		background-color: <?=$boxcolor['boxcolor']?>;
+		width: 200px;
+		height: 200px;
+		margin-left: 42%;
+		border: 5px solid black;
+	}
+</style>
