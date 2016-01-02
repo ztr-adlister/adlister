905a145ee89d88579dabb7978d107df516fb45ff
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-01-02 15:02:01 -0600

    extra css stuff

--- public/css/userbox.php
+++ public/css/userbox.php
@@ -1,12 +1,12 @@
 <?php
-$boxdata = $dbc->prepare('SELECT boxcolor FROM users WHERE username = :username');
-$boxdata->bindValue(':username', $_SESSION['Loggedinuser'], PDO::PARAM_STR);
-$boxdata->execute();
-$boxcolor = $boxdata->fetch(PDO::FETCH_ASSOC);
+// $boxdata = $dbc->prepare('SELECT boxcolor FROM users WHERE username = :username');
+// $boxdata->bindValue(':username', $_SESSION['Loggedinuser'], PDO::PARAM_STR);
+// $boxdata->execute();
+// $boxcolor = $boxdata->fetch(PDO::FETCH_ASSOC);
 ?>
 <style>
 #box {
-		background-color: <?=$boxcolor['boxcolor']?>;
+		/*background-color: <?=$boxcolor['boxcolor']?>;*/
 		width: 200px;
 		height: 200px;
 		margin-left: 42%;
