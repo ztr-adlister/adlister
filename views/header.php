2d433e7e35409fc114a852c662d016fc0ed3efa5
Anthony Burns <aburns42@gmail.com>
2016-01-02 13:31:52 -0600

    Updated header with three google fonts and minor changes to main.css page.

--- views/header.php
+++ views/header.php
@@ -10,5 +10,5 @@
     <link rel="stylesheet" href="../css/reagan.css">
     <link rel="stylesheet" href="../css/z.css">
     <link rel="stylesheet" type="text/css" href="/css/font-awesome-4.5.0/css/font-awesome.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Fugaz+One|Lobster' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Fugaz+One|Lobster|Shadows+Into+Light' rel='stylesheet' type='text/css'>
 </head>
