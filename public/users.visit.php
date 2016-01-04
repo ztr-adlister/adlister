ecbbe96bb842e0e83ef2846d3792cefcdac5b177
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-01-04 13:46:47 -0600

    finished making the profile page and visiting profile pages mobile-responsive

--- public/users.visit.php
+++ public/users.visit.php
@@ -35,9 +35,9 @@ if(User::finduserbyusername($usertovisit)) {
     <h2 class = "show textinfrontofbackground"><?=$visitinguser['username']?>'s Ads</h2>
     <div id = "box" style = "background-color: <?=$boxcolor?>;"><br><br><i id = "icon" class = "fa fa-<?=$visitinguser['icon']?> fa-5x"></i></div>
  <!-- Ads the user has posted -->
-    	<ul class = "showads">
-            <div class = "col-md-7 col-md-offset-2">
-                <br>
+ <div class = "row">
+    <div class ="col-md-8 col-md-offset-2 col-sm-7 col-sm-offset-2 showads">
+    	<ul><br>
             <?php foreach($visitingads as $advalue) {?>
                 <li><a class = "hidden-xs hidden-sm" href = "ads.show.php?id=<?=$advalue['id']?>"><img id = "image" src = "img/<?=$advalue['image_url']?>" class = "img-responsive" alt="Responsive image"></a></li>
             <li><strong>Title:</strong><a href = "ads.show.php?id=<?=$advalue['id']?>"><?=$advalue['title']?></a></li>
@@ -46,10 +46,15 @@ if(User::finduserbyusername($usertovisit)) {
             <br><br><br>
             <?php } ?>
             </div>
-        <br><br><br><br>
-        <button class = "btn btn-success hidden-xs hidden-sm"><a href="ads.index.php" id = "editads"><i class = "fa fa-pencil"></i>Back to Ads</a></button>
-        <br><br><br>
+            <div class = "row">
+                <div class = "col-md-6 col-md-offset-2">
+                    <button class = "btn btn-success"><a href="ads.index.php" id = "editads"><i class = "fa fa-pencil"></i>Back to Ads</a></button>
+                    <br><br><br>
+                </div>
+            </div>
         </ul>
-    	<br><br><br><br><br><br><br><br><br><br>
+    </div>
+ </div>
+    	
 </body>
 <?php require_once '../views/footer.php';?>
